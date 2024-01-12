5290632f2423ff638c90bc274e572f9814d0c192
Sergey Semyonov <evlx@mail.ru>
2024-01-12 21:20:32 +0300

    update catalog views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'page_title': 'Главная',
+        'objects_list': Product.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    context = {
+        'page_title': 'Контакты'
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}): {message}')
+    return render(request, 'contacts.html', context)
 
 
 def product(request):
-    return render(request, 'product.html')
+    context = {
+        'page_title': 'Товары',
+        'objects_list': Product.objects.all()
+    }
+    return render(request, 'product.html', context)
