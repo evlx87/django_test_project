3ff7cb8aebc66bddb38ce422062704d671f27c51
Sergey Semyonov <evlx@mail.ru>
2024-01-13 00:11:27 +0300

    add docstring in fill.py

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -8,13 +8,26 @@ from config import settings
 
 
 def load_from_json(file_name):
+    """
+    Загружает данные из JSON-файла.
+
+    :param file_name: имя файла без расширения
+    :return: данные из файла в формате словаря
+    """
     with open(os.path.join(settings.JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
         return json.load(infile)
 
 
 class Command(BaseCommand):
+    """Django команда для очистки и заполнения базы данных тестовыми товарами и категориями."""
 
     def handle(self, *args, **options):
+        """
+        Обработчик команды.
+
+        Очищает базу данных от существующих категорий и товаров,
+        загружает новые категории и товары из JSON-файлов.
+        """
         categories = load_from_json('categories')
 
         Category.objects.all().delete()
