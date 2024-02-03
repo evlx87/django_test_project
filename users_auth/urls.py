f4174ee8b7a1d1896debce79758503b2ae7ee2d9
evlx87 <evlx@mail.ru>
2024-02-03 20:55:58 +0300

    исправлена ошибка возникающая при выходе из профиля

--- users_auth/urls.py
+++ users_auth/urls.py
@@ -8,7 +8,7 @@ app_name = UsersAuthConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users_auth/login_form.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('verify_code/', VerifyCodeView.as_view(), name='verify_code'),
     # path('pass_reset/', PasswordResetView.as_view(), name='pass_reset'),
